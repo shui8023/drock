c4f1cead772f053171677080082db8434437a862
shui8023 <dubingyangxiyou@gmail.com>
2014-10-28 00:46:31 +0800

    实现自己的真正的os

---
+++ src/boot/asm.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  asm.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月27日 21时01分30秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Shui(dubingyang), dubingyangxiyou@gmail.com
+ *        Company:  Class 1201 of software engineering
+ *
+ * =====================================================================================
+ */
+
+
