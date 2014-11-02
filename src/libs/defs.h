939c83fe2cd7332baba619cbd36e4f8d210fdfa2
shui8023 <dubingyangxiyou@gmail.com>
2014-11-02 22:18:12 +0800

    elf文件格式

---
+++ src/libs/defs.h
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  defs.h
+ *
+ *    Description:  一些符号的定义
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月01日 23时11分32秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Shui(dubingyang), dubingyangxiyou@gmail.com
+ *        Company:  Class 1201 of software engineering
+ *
+ * =====================================================================================
+ */
+
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef 	int 		int32_t;
+typedef   	unsigned 	uint32_t;
+typedef 	short 		int16_t;
+typedef 	unsigned short  uint16_t;
+typedef 	char 		int8_t;
+typedef 	unsigned char 	uint8_t;
+
+#endif //DEFS_H
+
